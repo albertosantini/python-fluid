55227a8de515e577e32b4da8504d6730e2227c53
icebox <albertosantini@gmail.com>
2015-12-26 02:30:32 +0100

    Fix syntax error in demo.

--- src/demo.py
+++ src/demo.py
@@ -33,9 +33,7 @@ try:
     from OpenGL.GL import *
     from OpenGL.GLU import *
 except:
-    print '''
-ERROR: PyOpenGL not installed properly.
-        '''
+    print 'ERROR: PyOpenGL not installed properly.'
     sys.exit()
 
 N = 32
